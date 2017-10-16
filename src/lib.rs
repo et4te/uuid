0bd8d718c6b200268587d6ef7c06eb8f6d704fc8
Edward Tate <cmalune@gmail.com>
2017-10-16 16:49:30 -0400

    Use OsRng, 64bit nonce uses 56-bit nanosecs + r.

--- src/lib.rs
+++ src/lib.rs
@@ -10,9 +10,10 @@ use std::path::Path;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::ops::Add;
+use std::time::{SystemTime, Duration, UNIX_EPOCH};
 use std::ffi::CStr;
-use rand::Rng;
+use rand::{OsRng, Rng};
 use rustc_serialize::hex::FromHex;
 use eui48::{MacAddress, Eui48};
 
@@ -72,27 +73,31 @@ impl UUIDGen {
 
     // Generates a 64-bit nonce. This should not be used as a UUID.
     pub fn nonce64(&self) -> [u8; 8] {
-        let nanosec_bytes = nanosecs_since_unix_epoch();
+        let nanosec_bytes = nanosecs_since_epoch56();
 
-        let mac_bytes = self.0.as_bytes();
+        let mut rng = OsRng::new()
+            .expect("Failed to initialise RNG.");
+        let r = rng.gen::<[u8; 1]>();
+
         let mut bytes = [0; 8];
-        bytes[0] = nanosec_bytes[0] ^ mac_bytes[0];
-        bytes[1] = nanosec_bytes[1] ^ mac_bytes[1];
-        bytes[2] = nanosec_bytes[2] ^ mac_bytes[2];
-        bytes[3] = nanosec_bytes[3] ^ mac_bytes[3];
-        bytes[4] = nanosec_bytes[4] ^ mac_bytes[4];
-        bytes[5] = nanosec_bytes[5] ^ mac_bytes[5];
+        bytes[0] = nanosec_bytes[0];
+        bytes[1] = nanosec_bytes[1];
+        bytes[2] = nanosec_bytes[2];
+        bytes[3] = nanosec_bytes[3];
+        bytes[4] = nanosec_bytes[4];
+        bytes[5] = nanosec_bytes[5];
         bytes[6] = nanosec_bytes[6];
-        bytes[7] = nanosec_bytes[7];
+        bytes[7] = r[0];
         bytes
     }
 
     // A variant of the v1 UUID (128-bit).
     pub fn uuid128(&self) -> [u8; 16] {
-        let nanosec_bytes = nanosecs_since_unix_epoch();
+        let nanosec_bytes = nanosecs_since_epoch();
 
-        let mut rng = rand::thread_rng();
-        let r = rng.gen::<[u8; 8]>();
+        let mut rng = OsRng::new()
+            .expect("Failed to initialise RNG.");
+        let r = rng.gen::<[u8; 2]>();
 
         let mac_bytes = self.0.as_bytes();
         let mut bytes = [0; 16];
@@ -110,8 +115,8 @@ impl UUIDGen {
         bytes[11] = mac_bytes[3];
         bytes[12] = mac_bytes[4];
         bytes[13] = mac_bytes[5];
-        bytes[14] = r[6];
-        bytes[15] = r[7];
+        bytes[14] = r[0];
+        bytes[15] = r[1];
         bytes
     }
 }
@@ -119,6 +124,22 @@ impl UUIDGen {
 //------------------------------------------------------------------------------
 // Internal
 //------------------------------------------------------------------------------
+
+fn epoch_min() -> SystemTime {
+    let secs_years_47: u64 = 60 * 60 * 24 * 365 * 47;
+    UNIX_EPOCH.add(Duration::new(secs_years_47, 0))
+}
+
+// Takes the inverse of the little_endian function on a 64-bit integer
+// in order to obtain 7 bytes.
+fn little_endian_inv56(x: u64) -> [u8; 7] {
+    let mut bytes: [u8; 7] = [0; 7];
+    bytes[0] = (x & 0xFF) as u8;
+    for i in 1..7 {
+        bytes[i] = ((x & (0xFF << i * 8)) >> i * 8) as u8;
+    }
+    bytes
+}
 
 // Takes the inverse of the little_endian function on a 64-bit integer
 // in order to obtain 8 bytes.
@@ -131,15 +152,28 @@ fn little_endian_inv64(x: u64) -> [u8; 8] {
     bytes
 }
 
-// 100-nanosecond intervals since UNIX_EPOCH.
-fn nanosecs_since_unix_epoch() -> [u8; 8] {
+// 100-nanosecond intervals since 2017 as 56-bits.
+fn nanosecs_since_epoch56() -> [u8; 7] {
     let now = SystemTime::now();
-    let duration = now.duration_since(UNIX_EPOCH)
+    let duration = now.duration_since(epoch_min())
         .expect("Failed to get duration since unix epoch.");
 
     // seconds to 100-nanosecond interval bytes
     let secs: u64 = duration.as_secs() as u64 * 1_000_000_0;
-    let nanosecs: u64 = secs + (duration.subsec_nanos() as u64 / 1_00);
+    let nanosecs: u64 = secs + (duration.subsec_nanos() / 1_00) as u64;
+
+    little_endian_inv56(nanosecs)
+}
+
+// 100-nanosecond intervals since 2017 as 64-bits.
+fn nanosecs_since_epoch() -> [u8; 8] {
+    let now = SystemTime::now();
+    let duration = now.duration_since(epoch_min())
+        .expect("Failed to get duration since unix epoch.");
+
+    // seconds to 100-nanosecond interval bytes
+    let secs: u64 = duration.as_secs() as u64 * 1_000_000_0;
+    let nanosecs: u64 = secs + (duration.subsec_nanos() / 1_00) as u64;
 
     little_endian_inv64(nanosecs)
 }
@@ -175,10 +209,26 @@ pub fn read_interface_eui(iface: &str) -> Eui48 {
 
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    use super::*;
+    use std::collections::HashMap;
+    use rustc_serialize::hex::ToHex;
 
     #[test]
     fn uuid_gen() {
-        // ...
+
+        let eui = read_interface_eui("new0");
+        let gen = UUIDGen::new(eui);
+
+        // birthday paradox B ^ (1/2 * n) of 64 bits => 2 ^ 32
+        let mut entries = HashMap::new();
+
+        for i in 1..4_294_967_296 as u64 {
+            let key = gen.nonce64().to_hex();
+            if !entries.contains_key(&key) {
+                entries.insert(key, ());
+            } else {
+                println!("Found collision at {} where key = {}", i, key);
+            }
+        }
     }
 }
